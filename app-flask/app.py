869462ed096b990176f76be5d4809b83801b12eb
Austen <connect@austenvalentine.com>
2020-02-19 10:26:50 -0500

    setting up flask RESTful

--- app-flask/app.py
+++ app-flask/app.py
@@ -1,3 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api
+from resources.login import login
 
+app = Flask(__name__)
+api = Api()
+
+api.add_resource(login)
+
+if __name__=="__main__":
+  app.run(port=5000, debug=True)
---
+++ app-flask/resources/user.py
@@ -0,0 +1,4 @@
+from flask_restful import Resource
+class login(Resource):
+  def get(self):
+    return {"message": "Hello World!"}, 200
