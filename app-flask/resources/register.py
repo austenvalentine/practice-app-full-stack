bd118f7f0368692a575be5b76d185e31881d6b68
Austen <connect@austenvalentine.com>
2020-03-12 00:06:33 -0400

    moving encryption and text transformation into models

--- app-flask/resources/register.py
+++ app-flask/resources/register.py
@@ -1,5 +1,6 @@
 from models.user_model import UserModel
 from flask_restful import Resource, reqparse
+from validate_email import validate_email
 
 parser = reqparse.RequestParser()
 parser.add_argument("username", type=str, required=True, help="username is required" )
@@ -9,8 +10,29 @@ parser.add_argument("password", type=str, required=True, help="password is requi
 class Register(Resource):
   def post(self):
     args = parser.parse_args()
-    if UserModel.getUserByUsername():
-      return {"message": "error: username is taken"}
+    password = args["password"].lower()
+    if UserModel.user_exists(args["username"], args["email"]):
+      return {"message":"user exists"}, 400
+
+    if validate_email(email, verify=True):
+      console.log(">>>>>>>>>>>>>!!!!VALID EMAIL!")
     else:
-      return 400
+      return {"message": "invalid email address"}, 400
+
+    if len(args["password"]) < 10:
+      return {"message":"password must be at least 10 characters"}, 400
+
+    acc = {}
+    for ch in args["password"]:
+      if acc.get[ch, None]:
+        acc[ch]++
+      else: 
+        acc[ch] = 0
+      
+    if len(acc.key() < 6):
+      return {"message" : "password must contain at least 6 different symbols"}, 400
+
+
+    
+
 
